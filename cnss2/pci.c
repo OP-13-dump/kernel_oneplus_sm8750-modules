5cb69dbfa4139b7620fb982b103e9e36bd3c5f57
Yu Wang <yyuwang@qti.qualcomm.com>
2025-08-20 20:48:27 -0700

    cnss2: fix modpost error by dropping cma_get_name
    Previously, the module used cma_get_name() to log the
    name of the CMA area. This worked on Android Linux kernels
    where the symbol is exported.
    
    However, in upstream Linux kernels, cma_get_name() is not
    exported, leading to a modpost error during out-of-tree
    module build.
    
    To fix this, the call to cma_get_name() has been removed and
    replaced with a simple validity check on the CMA area.
    
    Change-Id: I5db4fca05cbad001a8d0853860ae68bc41a3a920
    CRs-Fixed: 4263707

--- cnss2/pci.c
+++ cnss2/pci.c
@@ -8455,9 +8455,9 @@ static int cnss_pci_of_reserved_mem_device_init(struct cnss_pci_data *pci_priv)
 			cnss_pr_err("Failed to init reserved mem device, err = %d\n",
 				    ret);
 	}
-	if (dev_pci->cma_area)
-		cnss_pr_dbg("CMA area is %s\n",
-			    cma_get_name(dev_pci->cma_area));
+
+	if (!dev_pci->cma_area)
+		cnss_pr_info("Invalid CMA area\n");
 
 	return ret;
 }
