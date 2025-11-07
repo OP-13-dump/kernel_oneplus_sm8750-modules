8cd87ad767e27528f1a014099412cf014796e07f
Syed Ahmed <syedahme@qti.qualcomm.com>
2025-11-07 12:34:20 +0530

    touch: monaco: Enable ddk compilation for monaco
    Enable DDK compilation for monaco
    
    Change-Id: I2a49291936c1a92043962d8b84fee6608d1bb716
    Signed-off-by: Syed Ahmed <syedahme@qti.qualcomm.com>

--- Android.mk
+++ Android.mk
@@ -41,6 +41,10 @@ ifeq ($(TARGET_BOARD_PLATFORM), art)
       LOCAL_MODULE_DDK_BUILD := true
 endif
 
+ifeq ($(TARGET_BOARD_PLATFORM), monaco)
+      LOCAL_MODULE_DDK_BUILD := true
+endif
+
 include $(CLEAR_VARS)
 
 # This makefile is only for DLKM
