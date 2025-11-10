95601548a22aec23e9dc9d67cda17f943849673d
Ayushi Makhija <quic_amakhija@quicinc.com>
2025-11-10 19:54:33 +0530

    touch: chora: Fix touch driver compilation for chora target
    Fix touch driver compilation for Chora target.
    
    Change-Id: Id1642b324cfc0a889e44fa994311543ac4fb3c99
    Signed-off-by: Ayushi Makhija <quic_amakhija@quicinc.com>

--- touch_driver_product.mk
+++ touch_driver_product.mk
@@ -30,6 +30,7 @@ ifeq ($(TOUCH_DLKM_ENABLE),  true)
                 PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/goodix_ts.ko \
                         $(KERNEL_MODULES_OUT)/atmel_mxt_ts.ko \
                         $(KERNEL_MODULES_OUT)/st_fts.ko \
+                        $(KERNEL_MODULES_OUT)/focaltech_fts.ko \
                         $(KERNEL_MODULES_OUT)/qts.ko
         else ifeq ($(TARGET_BOARD_PLATFORM), art)
                 PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/goodix_ts.ko \
