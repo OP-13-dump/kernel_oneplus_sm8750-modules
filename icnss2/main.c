2c1b0142022a8e299625ca23e3bda78ff4e65b95
Prateek Patil <pratpati@qti.qualcomm.com>
2026-01-08 14:04:44 +0530

    icnss2: Clear ICNSS_FW_READY bit during smmu fault handling
    At present, during handling of smmu fault, ICNSS_FW_READY bit was
    not cleared before sending FW_DOWN driver uevent.
    
    In case we recive FW_EARLY_CRASH_IND, this bit is getting cleared
    during handling the indication, before sending uevent to the driver
    which is a corrective step.
    
    We got a scenario where there is a race condition between SMMU Fault
    handling and FW_EARLY_CRASH_IND, because of which FW_DOWN driver uevent
    is sent twice to wlan host driver.
    
    This change clear ICNSS_FW_READY bit during smmu fault handling to avoid
    sending FW_DOWN uevent multiple times in such scenarios.
    
    Change-Id: I8c5f53955cfcd571dae3cf46544c287ef28c6a51
    CRs-Fixed: 4404844

--- icnss2/main.c
+++ icnss2/main.c
@@ -5934,6 +5934,7 @@ static int icnss_smmu_fault_handler(struct iommu_domain *domain,
 	}
 
 	if (test_bit(ICNSS_FW_READY, &priv->state)) {
+		clear_bit(ICNSS_FW_READY, &priv->state);
 		fw_down_data.crashed = true;
 		icnss_call_driver_uevent(priv, ICNSS_UEVENT_SMMU_FAULT,
 					 &fw_down_data);
@@ -5994,6 +5995,7 @@ static void icnss_pci_smmu_fault_handler_irq(struct iommu_domain *domain,
 
 	icnss_record_smmu_fault_timestamp(priv, SMMU_CB_ENTRY);
 	if (test_bit(ICNSS_FW_READY, &priv->state)) {
+		clear_bit(ICNSS_FW_READY, &priv->state);
 		fw_down_data.crashed = true;
 		icnss_call_driver_uevent(priv, ICNSS_UEVENT_SMMU_FAULT,
 					 &fw_down_data);
